d9f401f494b7cf950e49ae6c8466a4bf1c00a098
SpaceTimeWarrior <stevenjr@comporium.net>
2025-08-28 01:03:20 -0400

    chapter 3 main completion

---
+++ Tiled project/Temporal Light/in_game_HUD.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="in_game_HUD" tilewidth="16" tileheight="16" tilecount="10" columns="10">
+ <image source="in_game_HUD.png" width="160" height="16"/>
+</tileset>
